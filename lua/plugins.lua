44c9c07accbcde2f63fed52bbc80dccefd5b0295
nganhkhoa <mail.nganhkhoa@gmail.com>
2021-04-05 17:15:32 +0700

    add statusline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,6 +4,38 @@ packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'Olical/aniseed'
 
+  use {
+    'hoob3rt/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true},
+    config = function()
+      require('lualine').setup{
+        options = {
+          theme = 'nord',
+          section_separators = {'', ''},
+          component_separators = {'', ''},
+          icons_enabled = true,
+        },
+        sections = {
+          lualine_a = { {'mode', upper = true} },
+          lualine_b = { {'branch', icon = ''} },
+          lualine_c = { {'filename', file_status = true} },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location'  },
+        },
+        inactive_sections = {
+          lualine_a = {  },
+          lualine_b = {  },
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {  },
+          lualine_z = {   }
+        },
+        extensions = { 'fugitive' }
+      }
+    end
+  }
+
   use 'Yggdroot/indentLine'
   use 'ntpeters/vim-better-whitespace'
   use 'valloric/matchtagalways'
